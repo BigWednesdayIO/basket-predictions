c324460c1dda53646792d7347e37bb58c19b0bca
toboid <toboid@users.noreply.github.com>
2015-12-04 11:13:45 +0000

    Process file stream

--- test_model.js
+++ test_model.js
@@ -3,6 +3,9 @@
 'use strict';
 
 const program = require('commander');
+const rxNode = require('rx-node');
+const fs = require('fs');
+
 
 program
   .option('-t, --test-file [file]', 'Test file path')
@@ -16,15 +19,16 @@ if (!program.testFile) {
   });
 }
 
-console.log(program.model);
-console.log(program.project);
-console.log(program.testFile);
+console.log('Processing test file ', program.testFile);
 
+const subscription = rxNode.fromReadableStream(fs.createReadStream(program.testFile))
+    .map(t => t.toString().split('\n'))
+    .flatMap(t => t)
+    .take(10)
+    .subscribe(x => {
+      console.log(x);
+    });
 
-
-
-// Accepts a training file name, model name and project
 
 // Loops through training file, gets prediction for features, calculates accuracy.
   // Provides mean, mode, median and variance for accuracy
-
