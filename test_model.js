a59753b9aff9fceea9adbb436d36a3fd8ce30b05
toboid <toboid@users.noreply.github.com>
2015-12-04 12:25:13 +0000

    Get prediction for 1 case

--- test_model.js
+++ test_model.js
@@ -2,16 +2,19 @@
 
 'use strict';
 
+const fs = require('fs');
+const google = require('googleapis');
 const program = require('commander');
 const rxNode = require('rx-node');
-const fs = require('fs');
-
 
 program
+  .option('-m, --model [model]', 'Prediction model name')
+  .option('-p, --project [project]', 'GCloud project')
+  .option('-k, --key-file [file]', 'GCloud keyfile path')
   .option('-t, --test-file [file]', 'Test file path')
   .parse(process.argv);
 
-if (!program.testFile) {
+if (!program.model || !program.project || !program.testFile || !program.keyFile) {
   program.outputHelp(help => {
     console.log('  Missing command line args');
     console.log(help);
@@ -19,15 +22,36 @@ if (!program.testFile) {
   });
 }
 
-console.log('Processing test file ', program.testFile);
+const key = require(program.keyFile);
+
+var jwtClient = new google.auth.JWT(key.client_email, null, key.private_key, ['https://www.googleapis.com/auth/prediction'], null);
+
+const getPrediction = (features, callback) => {
+  google.prediction('v1.6').trainedmodels.predict({
+    auth: jwtClient,
+    id: program.model,
+    project: program.project,
+    resource: {input: {csvInstance: features}}
+  }, callback);
+}
 
 const subscription = rxNode.fromReadableStream(fs.createReadStream(program.testFile))
-    .map(t => t.toString().split('\n'))
-    .flatMap(t => t)
-    .take(10)
-    .subscribe(x => {
-      console.log(x);
+  .map(t => t.toString().split('\n'))
+  .flatMap(t => t)
+  .take(1)
+  .map(t => t.split(','))
+  .subscribe(x => {
+    const actualVolume = x[0];
+    const features = x.slice(1);
+
+    getPrediction(features, (err, result) => {
+      if (err) {
+        return console.error('Predict failed', err);
+      }
+
+      console.log(result.outputValue);
     });
+  });
 
 
 // Loops through training file, gets prediction for features, calculates accuracy.
